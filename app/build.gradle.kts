db36dcb0da72af4b86bd792dcaab5bd74614f15f
TeamShryne <TeamShryne@users.noreply.github.com>
2026-09-10 07:49:05 +0000

    Fix CI: versionless plugin ids resolved from root buildscript classpath

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("com.android.application") version "8.13.2"
-    id("org.jetbrains.kotlin.android") version "2.0.21"
+    // Versions come from the root buildscript classpath (shared with vendored modules).
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
